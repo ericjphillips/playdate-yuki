a70cd5936b7b311473d490b1864bdb3fda20fee5
Eric Phillips <ericj.phillips@gmail.com>
2016-07-03 13:18:51 -0400

    feat: return early from compare command in the case of missing data

--- modules/command.js
+++ modules/command.js
@@ -9,6 +9,9 @@ module.exports = {
     console.log(`Received compare request: compare ${message}`)
     let player1 = message.substring(0, message.indexOf(' to '))
     let player2 = message.substring(message.indexOf(' to ') + 4)
+    if (!player1 || !player2) {
+      return 'To compare, say: !compare player to player'
+    }
     let players = audience.filter((player) => {
       if (player.name === player1 || player.name === player2) {
         return true
@@ -16,6 +19,13 @@ module.exports = {
         return false
       }
     })
+    if (players.length < 2) {
+      if (players.length < 1) {
+        return 'There\'s nobody by those names here right now.'
+      } else {
+        return `I only know who ${players[0].name} is.`
+      }
+    }
     players.forEach((player) => {
       fetch(steamAPI('IPlayerService', 'GetOwnedGames', player.id))
       .then(res => { return res.json() })
@@ -29,6 +39,15 @@ module.exports = {
         }
       })
     })
+    if (!players[0].games || !players[1].games) {
+      if (!players[0].games && !players[1].games) {
+        return `I couldn't lookup games for either ${players[0].name} or ${players[1].name}.`
+      } else if (!players[0].games) {
+        return `I couldn't lookup games for ${players[0].name}.`
+      } else {
+        return `I couldn't lookup games for ${players[1].name}`
+      }
+    }
     return `I'll compare ${message} a little bit later.`
   }
 }
