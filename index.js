9fd9493c361b52996bdd52c815f7805209cb9275
Eric Phillips <ericj.phillips@gmail.com>
2017-04-25 19:16:34 -0400

    Add a database connection.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const Discord = require('discord.js')
-
 const yuki = new Discord.Client()
 
-yuki.on('ready', () => { console.log(`Logged in as ${yuki.user.username}!`)})
+const MongoClient = require('mongodb').MongoClient
+let db
+
+MongoClient.connect(process.env.DB_URL, (err, database) => {
+  if (err) { return console.log(err) }
+  else {
+    db = database
+    console.log(`MongoDB successfully set up.`)
+  }
+})
+
+yuki.on('ready', () => { console.log(`Logged in as ${yuki.user.username}.`)})
 
 yuki.on('message', msg => {
   if (msg.content === 'deez') { msg.reply('nuts!') }
